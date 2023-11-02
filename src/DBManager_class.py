25d4ae85f752bff78e5e8ee89dceee16fd696a34
Катя <mochalova.es@mail.ru>
2023-11-02 22:47:01 +0300

    Доработка КР

--- src/DBManager_class.py
+++ src/DBManager_class.py
@@ -4,7 +4,7 @@ from config import config
 
 class DBManager:
     """
-    Класс, который подключается к БД PostgreSQL и работает с ним
+    Класс, который подключается к БД PostgresSQL и работает с ним
     """
 
     def __init__(self, db_name, params=config()):
@@ -17,13 +17,12 @@ class DBManager:
         :return:
         """
         conn = psycopg2.connect(database=self.db_name, **self.params)
-        with conn.curs() as cur:
-            cur.execute('SELECT name_company, COUNT(*)'
-                           'FROM companies'
-                           'JOIN vacancies USING (company_id) '
-                           'GROUP BY name_company;')
+
+        with conn.cursor() as cur:
+            cur.execute("SELECT name_company, COUNT(*) FROM companies JOIN vacancies USING (company_id) GROUP BY name_company;")
 
             data = cur.fetchall()
+        cur.close()
         conn.close()
         return data
 
@@ -34,12 +33,11 @@ class DBManager:
         :return:
         """
         conn = psycopg2.connect(database=self.db_name, **self.params)
-        with conn.curs() as cur:
-            cur.execute('SELECT name_vacancy, name_company, salary, url'
-                           'FROM vacancies'
-                           'JOIN companies USING (company_id);')
+        with conn.cursor() as cur:
+            cur.execute("""SELECT name_vacancy, name_company, salary, url_vacancies FROM vacancies JOIN companies USING (company_id);""")
 
             data = cur.fetchall()
+        cur.close()
         conn.close()
         return data
 
@@ -49,12 +47,11 @@ class DBManager:
         :return:
         """
         conn = psycopg2.connect(database=self.db_name, **self.params)
-        with conn.curs() as cur:
-            cur.execute('SELECT AVG(salary_average)'
-                           'FROM vacancies'
-                           'WHERE salary_average > 0;')
+        with conn.cursor() as cur:
+            cur.execute("""SELECT AVG(salary) FROM vacancies WHERE salary > 0;""")
 
             data = cur.fetchall()
+        cur.close()
         conn.close()
         return data
 
@@ -64,29 +61,27 @@ class DBManager:
         :return:
         """
         conn = psycopg2.connect(database=self.db_name, **self.params)
-        with conn.curs() as cur:
-            cur.execute('SELECT * '
-                           'FROM vacancies'
-                           'WHERE salary > (SELECT AVG(salary) FROM vacancies);')
+        with conn.cursor() as cur:
+            cur.execute("""SELECT * FROM vacancies WHERE salary > (SELECT AVG(salary) FROM vacancies);""")
 
             data = cur.fetchall()
+        cur.close()
         conn.close()
         return data
 
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(self, word):
         """
         Метод получает список всех вакансий, в названии которых содержатся переданные в метод слова
         :return:
         """
         conn = psycopg2.connect(database=self.db_name, **self.params)
-        with conn.curs() as cur:
-            cur.execute(f"""
-                        SELECT * 
-                        FROM vacancies
-                        WHERE lower(title_vacancy) LIKE '%{keyword}%'
-                        OR lower(title_vacancy) LIKE '%{keyword}'
-                        OR lower(title_vacancy) LIKE '{keyword}%'""")
+        with conn.cursor() as cur:
+            cur.execute(f"""SELECT name_company, name_vacancy, salary, url_vacancies
+        FROM vacancies
+        JOIN companies USING (company_id)
+        WHERE name_vacancy LIKE '%{word.lower()}%' OR name_vacancy LIKE '%{word.title()}%' OR name_vacancy LIKE '%{word.upper()}%';""")
 
             data = cur.fetchall()
+            cur.close()
             conn.close()
             return data
--- utils.py
+++ utils.py
@@ -1,9 +1,5 @@
 import json
-import os
 import psycopg2
-from src.Vacancy_class import Vacancy
-
-vacancy = Vacancy()
 
 
 def load_jsonfile(filename: str):
@@ -15,7 +11,7 @@ def load_jsonfile(filename: str):
     return data
 
 
-def create_database(params, db_name) -> None:
+def create_database(db_name, params) -> None:
     """
     Создает новую базу данных.
     """
@@ -35,51 +31,49 @@ def create_tables(db_name, params):
     """
     conn = psycopg2.connect(database=db_name, **params)
 
-    with conn.curs() as cur:
+    with conn.cursor() as cur:
         cur.execute('CREATE TABLE companies('
-                       'company_id int PRIMARY KEY,'
-                       'name_company varchar(50) NOT NULL'
-                       'url_vacancies varchar(200) NOT NULL)')
+                    'company_id int PRIMARY KEY,'
+                    'name_company varchar(50) NOT NULL)')
 
         cur.execute('CREATE TABLE vacancies('
-                       'company_id int PRIMARY KEY REFERENCES companies (company_id) NOT NULL,'
-                       'name_vacancy varchar(150) NOT NULL,'
-                       'salary int,'
-                       'url varchar(200) NOT NULL,'
-                       'description text')
+                    'company_id int,'
+                    'name_vacancy varchar(150) NOT NULL,'
+                    'salary int,'
+                    'url_vacancies varchar(200) NOT NULL,'
+                    'description text)')
 
     conn.commit()
     conn.close()
 
 
-def insert_data_companies(db_name, params):
+def insert_data_companies(db_name, params, name_company, id_company):
 
     conn = psycopg2.connect(database=db_name, **params)
 
-    with conn.curs() as cur:
-        cur.execute('INSERT INTO companies (company_id, name_company, url_vacancies)'
-                    'VALUES (%s, %s, %s)'
+    with conn.cursor() as cur:
+        cur.execute('INSERT INTO companies (company_id, name_company)'
+                    'VALUES (%s, %s)'
                     'returning company_id',
-                     (int(data["employer"]["id"],
-                      data["employer"]["name"]),
-                      data["employer"]["alternate_url"]))
-
-        company_id = cur.fetchone()[0]
-
-        cur.execute('INSERT INTO vacancies (company_id, name_vacancy, salary, url, description)'
-                    'VALUES (%s, %s, %s, %s, %s)',
-                    (company_id,
-                     vacancy.name_vacancy,
-                     vacancy.salary_vacancy,
-                     vacancy.url_vacancy,
-                     vacancy.info_vacancy))
-
+                    (id_company,
+                     name_company))
     conn.commit()
     conn.close()
 
 
+def insert_data_vacancies(db_name, params, list_data):
 
+    conn = psycopg2.connect(database=db_name, **params)
 
+    with conn.cursor() as cur:
 
+        cur.execute('INSERT INTO vacancies (company_id, name_vacancy, salary, url_vacancies, description)'
+                    'VALUES (%s, %s, %s, %s, %s)',
+                    (list_data[0],
+                     list_data[2],
+                     list_data[4],
+                     list_data[3],
+                     list_data[5]))
 
-
+    conn.commit()
+    conn.close()
